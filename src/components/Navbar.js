8056ce7ce817d9425611148f23492c4126ed11bd
ffernandoVidal <ffervidal74@icloud.com>
2024-04-04 00:20:58 -0600

    se agrega el proyecto

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import "./Navstyl.css";
+
+
+function Navbar(){
+    return (
+        <>
+        
+        <nav>
+            <a></a>
+            <div>
+                <ul id="navbar">
+                    <li> <a className="active"href="index.html"> Inicio</a></li>
+                    <li> <a href="index.html"> Contactanos</a></li>
+                    <li> <a href="index.html"> Productos</a></li>
+                    <li> <a href="index.html"> Envios</a></li>
+                </ul>
+            </div>
+
+        </nav>
+        </>
+    )
+}
+
+export default Navbar; 
